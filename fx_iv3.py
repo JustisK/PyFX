d676664680340b2b1b90f4a3a18096aa45b98518
Keegan Justis <justisk@ou.edu>
2017-08-03 13:52:36 -0500

    whitespace unbuggereder

--- fx_iv3.py
+++ fx_iv3.py
@@ -10,8 +10,8 @@ from keras.models import Model
 from keras.layers import Input, Dense, Output, Flatten, Dropout
 
 def extract_features():
-	
-	# Load dataset (any image-based dataset)
+
+    # Load dataset (any image-based dataset)
     matches = [(re.match(r'^(([a-zA-Z]+)\d+\.png)', fname), path) 
 	    for path, dirs, files in os.walk('../') for fname in files]
     patches = [skimage.transform.resize( # resize image to (256, 256) TODO argv param based on patch size
