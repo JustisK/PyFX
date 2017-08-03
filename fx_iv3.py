2bf783d096b580afb25c684d369dde522968c182
Keegan Justis <justiskt@dev.cgi.missouri.edu>
2017-08-03 13:46:55 -0500

    whitespace unbuggered

--- fx_iv3.py
+++ fx_iv3.py
@@ -12,30 +12,30 @@ from keras.layers import Input, Dense, Output, Flatten, Dropout
 def extract_features():
 	
 	# Load dataset (any image-based dataset)
-	matches = [(re.match(r'^(([a-zA-Z]+)\d+\.png)', fname), path) 
-		for path, dirs, files in os.walk('../') for fname in files]
-	patches = [skimage.transform.resize( # resize image to (256, 256) TODO argv param based on patch size
-		skimage.io.imread(os.path.join(path, match.group(1))), # open each image
-    		(256, 256)) for match, path in matches if match]
+    matches = [(re.match(r'^(([a-zA-Z]+)\d+\.png)', fname), path) 
+	    for path, dirs, files in os.walk('../') for fname in files]
+    patches = [skimage.transform.resize( # resize image to (256, 256) TODO argv param based on patch size
+	    skimage.io.imread(os.path.join(path, match.group(1))), # open each image
+    	(256, 256)) for match, path in matches if match]
 	
 	# change color channel order and shift mean color for ImageNet		
-	patches = imagenet_utils.preprocess_input(np.array(patches)) 
-	labels = [match.group(2) for match, path in matches if match]
-	labels = LabelBinarizer().fit_transform(list(labels)) # one-hot encoding
-	print('patches', patches[0].shape, len(patches), 'labels', len(labels))
+    patches = imagenet_utils.preprocess_input(np.array(patches)) 
+    labels = [match.group(2) for match, path in matches if match]
+    labels = LabelBinarizer().fit_transform(list(labels)) # one-hot encoding
+    print('patches', patches[0].shape, len(patches), 'labels', len(labels))
 
 	# Construct model (using ImageNet weights)
-	self.inceptionV3 = InceptionV3(weights = "imagenet", include_top = False, 
-				pooling = avg, input_shape = patches[0].shape)
-		
-	for layer in inceptionV3.layers:
+    self.inceptionV3 = InceptionV3(weights = "imagenet", include_top = False, 
+        pooling = avg, input_shape = patches[0].shape)
+	
+    for layer in inceptionV3.layers:
 	    layer.trainable = False
-    
-	x = inceptionV3.outputs
-	x = Flatten()(x)
-	x = Dense(1024, activation="relu")(x)
-	x = Dropout(0.5)(x)
-	x = Dense(1024, activation="relu")(x)
+
+    x = inceptionV3.outputs
+    x = Flatten()(x)
+    x = Dense(1024, activation="relu")(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation="relu")(x)
     
     # actual feature extraction
     features = inceptionV3.predict(x, batch_size=1)
