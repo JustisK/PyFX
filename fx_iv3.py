fd993e41de9b07e8e73d76551f5e2dade008b57d
Keegan Justis <justiskt@dev.cgi.missouri.edu>
2017-08-03 13:42:06 -0500

    basic feature extraction implemented

--- fx_iv3.py
+++ fx_iv3.py
@@ -7,42 +7,35 @@ from sklearn.preprocessing import LabelBinarizer
 from keras.applications import InceptionV3
 from keras.applications import imagenet_utils
 from keras.models import Model
-from keras.layers import Flatten, Dense, Dropout
-from keras.optimizers import SGD, Adam
+from keras.layers import Input, Dense, Output, Flatten, Dropout
 
-# Load dataset (any image-based dataset)
-matches = [(re.match(r'^(([a-zA-Z]+)\d+\.png)', fname), path) for path, dirs, files in os.walk('./path/to/patches') for fname in files]
-patches = [skimage.transform.resize( # resize image to (256, 256) TODO argv param based on patch size
-        skimage.io.imread(os.path.join(path, match.group(1))), # open each image
-    (256, 256)) for match, path in matches if match]
-patches = imagenet_utils.preprocess_input(np.array(patches)) # change color channel order and shift mean color for ImageNet
-labels = [match.group(2) for match, path in matches if match]
-labels = LabelBinarizer().fit_transform(list(labels)) # one-hot encoding
-print('patches', patches[0].shape, len(patches), 'labels', len(labels)) # TODO figure out if labels are needed at all (shouldn't be)
+def extract_features():
+	
+	# Load dataset (any image-based dataset)
+	matches = [(re.match(r'^(([a-zA-Z]+)\d+\.png)', fname), path) 
+		for path, dirs, files in os.walk('../') for fname in files]
+	patches = [skimage.transform.resize( # resize image to (256, 256) TODO argv param based on patch size
+		skimage.io.imread(os.path.join(path, match.group(1))), # open each image
+    		(256, 256)) for match, path in matches if match]
+	
+	# change color channel order and shift mean color for ImageNet		
+	patches = imagenet_utils.preprocess_input(np.array(patches)) 
+	labels = [match.group(2) for match, path in matches if match]
+	labels = LabelBinarizer().fit_transform(list(labels)) # one-hot encoding
+	print('patches', patches[0].shape, len(patches), 'labels', len(labels))
 
-"""
-# Construct model (using ImageNet weights)
-inceptionV3 = InceptionV3(weights = "imagenet", include_top = False, input_shape = patches[0].shape)
-# for layer in inceptionV3.layers:
-#     layer.trainable = False
-x = inceptionV3.output
-x = Flatten()(x)
-x = Dense(1024, activation="relu")(x)
-x = Dropout(0.5)(x)
-x = Dense(1024, activation="relu")(x)
-"""
-
-# TODO feature extraction FROM LAYER BEHIND SOFTMAX CLASSIFIER
-
-features = inceptionV3.predict(x) # no idea if this will actually work
-print(features[:25])
-
-# END FEATURE EXTRACTION
-predictions = Dense(21, activation="softmax")(x)
-model = Model(inputs = [inceptionV3.input], outputs = [predictions])
-model.compile(loss = "categorical_crossentropy", optimizer = Adam(lr=0.0001), metrics=["accuracy"])
-model.summary()
-
-# Train model
-model.fit(x=patches, y=labels, batch_size=16, epochs = 10, validation_split=0.1, shuffle=True)
-
+	# Construct model (using ImageNet weights)
+	self.inceptionV3 = InceptionV3(weights = "imagenet", include_top = False, 
+				pooling = avg, input_shape = patches[0].shape)
+		
+	for layer in inceptionV3.layers:
+	    layer.trainable = False
+    
+	x = inceptionV3.outputs
+	x = Flatten()(x)
+	x = Dense(1024, activation="relu")(x)
+	x = Dropout(0.5)(x)
+	x = Dense(1024, activation="relu")(x)
+    
+    # actual feature extraction
+    features = inceptionV3.predict(x, batch_size=1)
