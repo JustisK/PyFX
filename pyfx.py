53dea8e294db24f3f1f93cbcc48784b2720ac381
Keegan Justis <justisk@ou.edu>
2017-08-07 10:10:26 -0500

    save to hdf5 implemented, argv needs updated w/ option for .csv/.txt or hdf

--- pyfx.py
+++ pyfx.py
@@ -1,11 +1,12 @@
 import os, sys, re
 import argparse
 import numpy as np
-import common
+import h5py
 import skimage.io
 import skimage.transform
 from sklearn.preprocessing import LabelBinarizer
 from keras.applications import InceptionV3
+from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Input, Dense, Flatten, Dropout, Reshape
@@ -21,41 +22,55 @@ parser.add_argument(nargs='?', type=str, dest='img_path',
                     default='./images', action='store')
 parser.add_argument(nargs='?', type=str, dest='out_path',
                     default='./output/features', action='store')
-parser.add_argument(nargs='?', type=str, dest='ext',
-                    default='csv', action='store')
+#parser.add_argument(nargs='?', type=str, dest='ext',
+                    #default='csv', action='store')
 args = parser.parse_args()
 
 def extract_features():
 
     # Load dataset (any image-based dataset)
-    matches = [(re.match(r'^(([a-zA-Z]+)\d+\.png)', fname), path) for path, dirs, files in os.walk(''+str(args.img_path)) for fname in files]
+    matches = [(re.match(r'^(([a-zA-Z]+)\d+\.png)', fname), path) 
+    		for path, dirs, files in os.walk(''+str(args.img_path)) 
+    		for fname in files]
     patches = [skimage.transform.resize( # resize image to (256, 256)
     	skimage.io.imread(os.path.join(path, match.group(1))), # open each image
 	    (256, 256)) for match, path in matches if match]
-   
-   # Preprocess for InceptionV3	
-    for img in patches:
-    	img = np.expand_dims(img, axis=0)
-    	img = InceptionV3.preprocess_input(img)
+    
+    # Preprocess for InceptionV3	
+    patches = preprocess_input(np.array(patches))
 
     # Construct model (using ImageNet weights)
     inceptionV3 = InceptionV3(weights = "imagenet", include_top = False, 
         input_shape = patches[0].shape)
-
+    
+    # Isolate pre-softmax outputs
     x = inceptionV3.output
 
+    """
+    # Fluff
+    x = Flatten()(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+	"""
+
     # Construct extractor model
-    extractor = Model(inputs=[inceptionV3.input],outputs=[x])
+    extractor = Model(inputs=[inceptionV3.input], outputs=[x])
 
     # Extract features with Model.predict()
     features = extractor.predict(x=patches, batch_size=2)
 
-    # Concatenate features to 1d array
-    features = np.ndarray.flatten(features)
+    # TODO: get rid of zero-padding
+    # TODO: concatenate individual patches to individual 1d arrays?
 
     return features
 
 features = extract_features()
-np.savetxt("" + str(args.out_path) + "." + str(args.ext), features, fmt='%f')
+# save by pickling (not recommended):
+# np.save(file=str(args.out_path), allow_pickle=True, arr=features)
+
+# (Recommended) save --> .hdf
+f = h5py.File(""+str(args.out_path)+".hdf5", "w")
+hdf = f.create_dataset(name=str(args.out_path), data=features)
 
 import gc; gc.collect()
