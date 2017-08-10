4b97424390621a280e484cb977348e174a68890c
Keegan Justis <justisk@ou.edu>
2017-08-10 13:24:06 -0500

    extract_single, less hardcoding in save_features

--- pyfx.py
+++ pyfx.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 import skimage.io
 import skimage.transform
+from sklearn.feature_extraction.image import extract_patches_2d
 from keras.applications import InceptionV3
 from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
@@ -27,12 +28,13 @@ def collect_args():
     parser.add_argument(nargs='?', type=bool, dest='compressed',
                         default=False, action='store')
     argv = parser.parse_args()
-
+    compressed = argv.compressed
+    extension = argv.ext
     # TODO: put this warning somewhere else
-    if argv.ext != ("csv" or "txt"):
+    if extension != ("csv" or "txt"):
         print("""WARNING: non-text output (bin, npy, hdf5) is incompressible for now.
         \nOutput will not be compressed.""")
-    elif not argv.compressed:
+    elif not compressed:
         print("""WARNING: non-compressed csv output is extremely large.
         Recommend re-running with compressed=True.""")
     return argv
@@ -77,7 +79,6 @@ def extract_multi():
     features = extractor.predict(x=patches, batch_size=2)
 
     # TODO: get rid of zero-padding
-    # TODO: concatenate individual patches to individual 1d arrays?
 
     return features
 
@@ -97,7 +98,38 @@ def extract_single():
     :return: Keras tensor containing extracted features.
     """
 
+    # Load dataset (any image-based dataset)
     target = image.load_image(args.img_path)
+    patches = extract_patches_2d(target)
+
+    # TODO: patch target --> patches[]
+    """
+    patches = [skimage.transform.resize(  # resize image to (256, 256)
+        skimage.io.imread(os.path.join(path, match.group(1))),  # open each image
+        (256, 256)) for match, path in matches if match]
+    """
+    # Preprocess for InceptionV3
+    patches = preprocess_input(np.array(patches))
+
+    # Construct model (using ImageNet weights)
+    inceptionV3 = InceptionV3(weights="imagenet", include_top=False,
+                              input_shape=patches[0].shape)
+
+    # Isolate pre-softmax outputs
+    x = inceptionV3.output
+
+    # Experimental - flatten to 2d for CSV
+    if args.ext == "csv":
+        x = Flatten()(x)
+
+    # Construct extractor model
+    extractor = Model(inputs=[inceptionV3.input], outputs=[x])
+
+    # Extract features with Model.predict()
+    features = extractor.predict(x=patches, batch_size=2)
+    # TODO: get rid of zero-padding
+
+    return features
 
 
 def extract_single_1d():
@@ -113,30 +145,34 @@ def extract_single_1d():
     target = image.load_img(args.img_path)
 
 
-def save_features():
+def save_features(*kwargs):
     """
     Writes extracted feature vectors into a binary or text file, per args.
     :return: 
     """
     features = extract_multi()
     print(features.shape)  # comment out if you don't care to know output shape
-
+    if kwargs == 'multi':
+        features = extract_multi()
+    elif kwargs == 'single':
+        features = extract_single()
+    # TODO: extract_1d
     extension = str(args.ext)
     compressed = args.compressed
-
+    out_path = str(args.out_path)
     # TODO: figure out compression for file types other than txt/csv
 
     if extension == "hdf5":
         # (Recommended, default) save --> .hdf
-        f = h5py.File("" + str(args.out_path) + ".hdf5", "w")
-        hdf = f.create_dataset(name=str(args.out_path), data=features)
+        f = h5py.File("" + out_path + ".hdf5", "w")
+        f.create_dataset(name=str(args.out_path), data=features)
     elif extension == "npy":  # god please don't actually do this
-        outfile = "" + str(args.out_path)
+        outfile = "" + out_path
         np.save(file=outfile, allow_pickle=True, arr=features)
     else:
         if compressed:
             extension += ".gz"
-        outfile = "" + str(args.out_path) + "." + extension
+        outfile = "" + out_path + "." + extension
         np.savetxt(fname=outfile, X=features, fmt='%1.6f')
     # TODO: (distant future) npz for the optional list of concat. 1d arrays
 
